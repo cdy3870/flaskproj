ead565a6b8319ab73d24d057f50e123c3eda8091
Calvin <cdy3870@gmail.com>
2020-01-04 01:25:53 -0500

    requests

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, redirect, request, session, f
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+import requests
 
 app = Flask(__name__)
 
@@ -25,7 +26,7 @@ def login_required(f):
 			return redirect(url_for('login'))
 	return wrap
 
-@app.route('/')
+'''@app.route('/')
 @login_required
 def home():
 	#g stores temporary objects, specific to flask, storing connection
@@ -36,7 +37,27 @@ def home():
 	#posts = [dict(title = row[0], description = row[1]) for row in cur.fetchall()]
 	#g.db.close()
 	posts = db.session.query(BlogPosts).all()
-	return render_template('index.html', posts = posts)
+	return render_template('index.html', posts = posts)'''
+
+@login_required
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+	results = []
+
+	if request.method == 'POST':
+		print('test')
+		errors = []
+
+		try:
+			url = request.form['url']
+			r = requests.get(url)
+			return redirect(url_for('wordcounter'))
+		except:       	
+			errors.append("Unable to get URL. Please make sure it's valid and try again.")
+			return {"error": errors}
+
+	posts = db.session.query(BlogPosts).all()
+	return render_template('index.html', posts = posts, results = results)
 
 @app.route('/welcome')
 def welcome():
@@ -53,19 +74,27 @@ def login():
 			session['logged_in'] = True
 			flash('you were just logged in')
 			return redirect(url_for('home'))
+			
 
 	return render_template('login.html', error=error)
+
+	
 
 @login_required
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
 	flash('you were just logged out')
-	return redirect(url_for('welcome'))
+	return redirect(url_for('login'))
 
 #established connection with database
 def connect_db():
 	return sqlite3.connect(app.database)
+
+@login_required
+@app.route('/wordcounter')
+def wordcounter():
+	return render_template('wordcount.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
